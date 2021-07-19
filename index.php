01d88530081f858366b9c24c5752f19cadc8c194
StevenShih-0402 <steven0210423@gmail.com>
2021-07-19 16:53:15 +0800

    CSS新增
    超帥

--- index.php
+++ index.php
@@ -5,30 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>查水表</title>
+    <link rel="stylesheet" href="style.css">
     <script src="jquery-3.6.0.min.js"></script>
     <script src="main.js"></script>
 </head>
 <body>
-    <h1>聯絡人管理</h1>
-    <hr>
+    <div class="watch">
+        <h1>聯絡人管理</h1>
 
-    <form action="" id="contact">
-        <div>
-            <label for="name">姓名：</label>
-            <input type="text" name="name" id="name"/>
-        </div><br>
+        <form action="" id="contact">
+            <div>
+                <label for="name">姓名：</label>
+                <input type="text" name="name" id="name"/>
+            </div><br>
 
-        <a href="#" class="getnum">查詢電話：</a>
-        <br><br>
+            <a href="#" class="getnum">查詢電話</a>
+            <br><br>
 
-        <div>
-            <label for="phone">電話：</label>
-            <input type="text" name="phone" id="phone">
-        </div><br>
+            <div>
+                <label for="phone">電話：</label>
+                <input type="text" name="phone" id="phone">
+            </div><br>
 
-        <input type="submit" value="新增聯絡人"/>
-    </form>
+            <input type="submit" value="新增聯絡人"/>
+        </form>
     
-    <div id="msg" class="box"></div>
+        <div id="msg" class="box"></div>
+    </div>
+    
 </body>
 </html>
